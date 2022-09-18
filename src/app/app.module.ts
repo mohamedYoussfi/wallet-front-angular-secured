e8dc931dc7d88f1ff5d16bee5ad4c660fd7557fc
mohamedYoussfi <med@youssfi.net>
2022-09-18 21:30:53 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {CurrenciesComponent} from "./currencies/currencies.component";
+import {ContinentsComponent} from "./continents/continents.component";
+import {WalletsComponent} from "./wallets/wallets.component";
+import {WalletTransactionsComponent} from "./wallet-transactions/wallet-transactions.component";
+import {CurrencyDepositComponent} from "./currency-deposit/currency-deposit.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CurrenciesComponent,
+    ContinentsComponent,
+    WalletsComponent,
+    WalletTransactionsComponent,
+    CurrencyDepositComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
